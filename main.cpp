791b6bb2c141ac4d42cc90f91ccf52f0aec3438a
Jada Smith <jada.smith@lethpolytech.ca>
2024-10-28 09:11:02 -0600

    skibidi

--- main.cpp
+++ main.cpp
@@ -17,8 +17,13 @@ int main() {
 	j = input.length() - 1;  // returns the size of the string. "bob" is a length of 3 
 
 	while ((i < j) && isPalindrome) {
-		isPalindrome = (input[i] == input[j]);
-		i++; j--; // i and j will move towards the middle
+		
+		if (input[i] == ' ') { i++; }// checks for a space and if there is one it skips it.
+
+		else if (input[j] == ' ') { j--; }//checks for a space
+
+		else { isPalindrome = (input[i++] == input[j--]); } // i and j will move towards the middle and check 2 chars
+		
 	}
 	
 	if (isPalindrome) {
