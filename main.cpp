3fbf1311597f5ac75a617aaa6e983690f92707c8
Jada Smith <jada.smith@lethpolytech.ca>
2024-10-28 08:56:16 -0600

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//write a program that checks if the user inputs a sentence that is a palindrome (same backwards as it is forwards)
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+
+	return 0; 
+}
