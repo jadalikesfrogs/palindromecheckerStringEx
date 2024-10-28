3860f981dda231b5884a39b02826b38b6a87d077
Jada Smith <jada.smith@lethpolytech.ca>
2024-10-28 09:04:19 -0600

    yuh

--- main.cpp
+++ main.cpp
@@ -7,6 +7,25 @@ using namespace std;
 
 int main() {
 
+	string input;
+
+	int i = 0; //pointer that goes from left to the right
+	int j = 0; //pointer that goes from right to the left 
+	bool isPalindrome = true;
+	
+	getline(cin, input); 
+	j = input.length() - 1;  // returns the size of the string. "bob" is a length of 3 
+
+	while ((i < j) && isPalindrome) {
+		isPalindrome = (input[i] == input[j]);
+		i++; j--; // i and j will move towards the middle
+	}
+	
+	if (isPalindrome) {
+		cout << "Yes" << endl; 
+	}
+	else { cout << "No" << endl; }
+
 
 	return 0; 
 }
